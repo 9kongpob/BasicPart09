0a85131b028c5190db1864c20b91606f94bf051e
9kongpob <k.kunchanuan@hotmail.com>
2019-11-19 15:20:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicPart09_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
